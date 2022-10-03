f2aa833b0496bf9a47945ed2b4a25ff112f35a7c
Aaaxyw <2939166862@qq.com>
2022-10-03 11:53:33 +0800

    排序

---
+++ 268.丢失的数字.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=268 lang=javascript
+ *
+ * [268] 丢失的数字
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+    nums.sort((a, b) => a - b);
+    if (nums[0] != 0) {
+        return 0;
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] + 1 != nums[i + 1]) {
+            return nums[i] + 1;
+        }
+    }
+};
+// @lc code=end
