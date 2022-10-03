21784050ad123025dec34e0d3d5a1359d012fd9e
Aaaxyw <2939166862@qq.com>
2022-10-03 11:59:14 +0800

    原地哈希

--- 268.丢失的数字.js
+++ 268.丢失的数字.js
@@ -10,13 +10,9 @@
  * @return {number}
  */
 var missingNumber = function (nums) {
-    nums.sort((a, b) => a - b);
-    if (nums[0] != 0) {
-        return 0;
-    }
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] + 1 != nums[i + 1]) {
-            return nums[i] + 1;
+    for (let i = 0; i < nums.length + 1; i++) {
+        if (!nums.includes(i)) {
+            return i;
         }
     }
 };
